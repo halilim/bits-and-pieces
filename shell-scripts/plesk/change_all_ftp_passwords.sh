4b69cc23f3f25b57eaab4625c2d3fb838ae45a4c
Halil Ozgur <halil.ozgur@gmail.com>
2011-11-16 17:39:42 +0200

    change all plesk ftp passwords

---
+++ shell-scripts/plesk/change_all_ftp_passwords.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# http://8bitpipe.com/?p=293
+# Generates 16 character passwords. Puts all logins and passwords into ftp_passwords file.
+for i in $(mysql -NB psa -uadmin -p`cat /etc/psa/.psa.shadow` -e 'select login from sys_users;'); do export PSA_PASSWD="$(openssl rand 12 -base64)"; /usr/local/psa/admin/bin/usermng --set-user-passwd --user=$i; echo "$i: $PSA_PASSWD" >> ftp_passwords; done
