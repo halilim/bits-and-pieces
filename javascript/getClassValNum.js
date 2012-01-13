9fd3c4249ce4de9a85cb4d78301c0b98e8b658f5
Halil Ozgur <halil.ozgur@gmail.com>
2012-01-13 10:33:12 +0200

    Slash escape... How could this be overlooked?

--- javascript/getClassValNum.js
+++ javascript/getClassValNum.js
@@ -3,7 +3,7 @@
  * <div class="test etc_vs">... getClassNum(".test", "etc") -> "vs"
  */
 function getClassVal(el, str) {
-    var re = new RegExp((str || "[^\s]+") + "_([^\s]+)");
+    var re = new RegExp((str || "[^\\s]+") + "_([^\\s]+)");
     var ret = null;
     if ($(el).size() > 0 && $(el).attr("class")) {
         var ret = re.exec($(el).attr("class"));
