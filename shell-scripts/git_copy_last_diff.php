2d0c95afe7b9ea8604d49a5c3ad797d149e35500
Halil Ozgur <halil.ozgur@gmail.com>
2012-06-10 22:19:36 +0300

    git_copy_last_diff

---
+++ shell-scripts/git_copy_last_diff.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * @author Halil Özgür <halil.ozgur@gmail.com>
+ * @copyright 2012
+ *
+ * Copy the changed/added files in the last git commit
+ * Usage:
+ *     php "<path>/git_copy_last_diff.php" "<repo path>" "<export path>"
+ *
+ * In Registry:
+ *     [HKEY_CLASSES_ROOT\Directory\shell\git_copy_last_diff]
+ *     @="Git &Export Last Diff"
+ *     command:
+ *     cmd /C <usage> (change /'s with \'s)
+ */
+
+define('DS', DIRECTORY_SEPARATOR);
+
+function mycopy($src, $dst)
+{
+    $dir = dirname($dst);
+    if (!is_dir($dir)) {
+        mkdir($dir, 0755, true);
+    }
+    return copy($src, $dst);
+}
+
+//echo '<pre>'.var_export($argv, true).'</pre>';
+
+$source = rtrim($argv[1], "/\\");
+$target = rtrim($argv[2], "/\\");
+chdir($source);
+exec("git diff-tree -r --no-commit-id --name-only --diff-filter=ACMRT HEAD~1 HEAD", $files);
+foreach ($files as $v) {
+    mycopy($source.DS.$v, $target.DS.$v);
+}
